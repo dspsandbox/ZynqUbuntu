5c509047c79cb6883baa0c8536aaa0aa3de4f068
Pau Gomez <pgomez@quside.net>
2020-03-18 12:20:56 +0100

    Update

---
+++ ZCU111/Vivado/ZCU111/ZCU111.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,23 @@
+--Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+--Date        : Wed Mar 18 10:54:12 2020
+--Host        : QSWS10 running 64-bit major release  (build 9200)
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  end component design_1;
+begin
+design_1_i: component design_1
+ ;
+end STRUCTURE;
